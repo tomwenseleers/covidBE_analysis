4eccb403d18fd8d1e4a88c35e9abad879ef7efa6
Tom Wenseleers <Tom.wenseleers@bio.kuleuven.be>
2020-10-15 12:13:36 +0200

    added stacked plot by age

--- scripts/plotCasesbyAge.R
+++ scripts/plotCasesbyAge.R
@@ -4,27 +4,43 @@
 
 source("scripts/downloadData.R")
 
-tmp <- full_join(filter(rawcases,
-                        SEX == "All",
-                        (AGEGROUP != "All") & (AGEGROUP != "unknown")),
-                 rawhospit,
-                 by = c("PROVINCE","REGION","DATE")) %>%
-  filter(PROVINCE == "All")
-
-slice <- tmp %>%
+slice = function(from=as.Date("2020-07-1"), to=Sys.Date()-3) { 
+  from = as.Date(from)
+  to = as.Date(to)
+  tmp = full_join(filter(rawcases,
+                          SEX == "All",
+                          (AGEGROUP != "All") & (AGEGROUP != "unknown")),
+                   rawhospit,
+                   by = c("PROVINCE","REGION","DATE")) %>%
+    filter(PROVINCE == "All")
+  df = tmp %>%
   mutate(DATE = as.Date(DATE)) %>%
   filter(REGION == "Belgium",
-         between(DATE, as.Date("2020-07-1"),
-                 Sys.Date()-3)) %>%
+         between(DATE, from, to)) %>%
   na.omit()
+  df$AGEGROUP = factor(df$AGEGROUP, levels=rev(unique(df$AGEGROUP)))
+  return(df) }
 
-slice$AGEGROUP = factor(slice$AGEGROUP, levels=rev(unique(slice$AGEGROUP)))
 
+
+# increase of confirmed nr of cases (loess smoothed) in different age categories over time
 library(ggthemes)
-qplot(data=slice, x=DATE, y=CASES, colour=AGEGROUP, geom="smooth", 
+data = slice(from=as.Date("2020-07-1"), to=Sys.Date()-3)
+qplot(data=data, x=DATE, y=CASES, colour=AGEGROUP, geom="smooth", 
       method="loess", # or method="gam", formula = y ~ s(x, bs = "cs"),
       se=FALSE, lwd=I(1.2)) + scale_y_log10() +
   scale_color_discrete(h=c(270, 0), c=150) +
   guides(color = guide_legend(reverse = TRUE)) +
   theme_hc() 
 ggsave("cases_by_age.png", width = 7, height = 5)
+
+
+# stacked column plot of age distribution over time using raw data
+data = slice(from=as.Date("2020-03-1"), to=Sys.Date()-3)
+ggplot(data=data, aes(x=DATE, y=CASES, colour=AGEGROUP, fill=AGEGROUP)) + geom_col(position = "fill") + 
+  scale_color_discrete(h=c(270, 0), c=150) +
+  scale_fill_discrete(h=c(270, 0), c=150)  +
+  guides(color = guide_legend(reverse = TRUE), fill = guide_legend(reverse = TRUE)) +
+  ylab("PROPORTION OF NEW CONFIRMED CASES") +
+  theme_hc() 
+ggsave("cases_by_age_stacked.png", width = 7, height = 5)
